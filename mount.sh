39e74b071e467dc9e6c340ad5c775228c60ead84
Edgar Kosulin <kosulin.siteko@gmail.com>
2022-12-15 22:55:18 +0300

    fsafsdf

--- mount.sh
+++ mount.sh
@@ -14,3 +14,4 @@ mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@home /de
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@log /dev/nvme0n1p4 /mnt/var/log
 mount /dev/nvme0n1p3 /mnt/win10
 mount /dev/nvme0n1p1 /mnt/boot/efi
+
