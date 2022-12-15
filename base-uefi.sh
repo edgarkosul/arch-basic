f7c80c97cb059a30039be0e08cfda40241860648
Edgar Kosulin <kosulin.siteko@gmail.com>
2022-12-15 22:55:30 +0300

    asdfasd

--- base-uefi.sh
+++ base-uefi.sh
@@ -11,15 +11,11 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
 pacman -S grub os-prober efibootmgr pulseaudio networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
-# pacman -S --noconfirm xf86-video-amdgpu
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux 
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -28,7 +24,7 @@ systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable tlp 
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
@@ -42,7 +38,7 @@ usermod -aG wheel edgar
 echo "edgar ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/edgar
 
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+printf "Done! Type exit, umount /mnt and reboot."
 
 
 
