583959dadb28f7cdb9c8ef868b9044fa9c40d0d8
Edgar Kosulin <kosulin.siteko@gmail.com>
2022-12-16 03:05:29 +0300

    jelwrjwj

--- mount.sh
+++ mount.sh
@@ -8,8 +8,8 @@ btrfs subvolume create @home
 btrfs subvolume create @log
 cd
 umount /mnt
-mkdir -p /mnt/{boot/efi,home,var/log,win10}
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@ /dev/nvme0n1p4 /mnt
+mkdir -p /mnt/{boot/efi,home,var/log,win10}
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@home /dev/nvme0n1p4 /mnt/home
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@log /dev/nvme0n1p4 /mnt/var/log
 mount /dev/nvme0n1p3 /mnt/win10
