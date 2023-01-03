481295d9cce7462f6f793025237cabff6620c0f7
Edgar Kosulin <kosulin.siteko@gmail.com>
2023-01-03 18:36:54 +0300

    add archinstall json

--- kde.sh
+++ kde.sh
@@ -3,7 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -c Finland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
@@ -21,8 +21,6 @@ makepkg -si --noconfirm
 
 
 sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
-
-sudo flatpak install -y spotify
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
